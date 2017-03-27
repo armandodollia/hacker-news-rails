e45505aa703f4ac95677abd2b3618bd8e42b23c9
Armando Dollia <dollia.armando@gmail.com>
2017-03-27 16:29:36 -0500

    Add user model and migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
-  resources :posts, only: [:index, :show, :new, :create, :destroy] do
+  resources :posts, only: [:index, :show, :create, :destroy] do
     member do
       resources :votes, only: [:create]
     end
