2bb9de723267a886e0b7e2d52aba62c3683269c7
Armando Dollia <dollia.armando@gmail.com>
2017-03-27 14:27:35 -0500

    Add ability to create new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,19 @@ class PostsController < ApplicationController
   end
   
   def create
-    
+    params[:post][:username] = Faker::Internet.user_name
+    params[:post][:comment_count] = rand(1000)
+    new_post = Post.new(post_params)
+    if new_post.save
+      :ok
+      if request.xhr?
+        render_partial '/posts/post', locals: {post: new_post}, layout: false
+      else
+        redirect_to '/posts'
+      end
+    else
+      :unprocessable_entity
+    end
   end
   
   def destroy
@@ -17,6 +29,11 @@ class PostsController < ApplicationController
   
   def show
     
+  end
+  
+  private
+  def post_params
+    params.require(:post).permit(:title, :username, :comment_count)
   end
   
 end
